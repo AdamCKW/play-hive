760d18e090c03a11de12d2e4ac8a244e18071e37
Adam Chong <adamkwchong@gmail.com>
2023-10-05 15:04:58 +0800

    add utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -60,3 +60,66 @@ export function formatTimeToNow(date: Date): string {
         },
     });
 }
+
+export const transformObject = (obj: any) => {
+    const { likes, _count, ...rest } = obj;
+
+    const transformed = {
+        ...rest,
+        likedByUser: likes?.length > 0,
+        likesCount: _count?.likes,
+        childrenCount: _count?.children,
+    };
+
+    if (obj.children) {
+        transformed.children = obj.children.map((child: any) =>
+            transformObject(child),
+        );
+    }
+
+    if (obj.parent) {
+        transformed.parent = transformObject(obj.parent);
+    }
+
+    return transformed;
+};
+
+export const removeHtmlTags = (input: string) => {
+    return input.replace(/<[^>]*>/g, "");
+};
+
+export const splitDate = (date: string) => {
+    const dateObject = new Date(date);
+
+    // Options for formatting the date
+    const options = {
+        year: "numeric" as const,
+        month: "long" as const,
+        day: "numeric" as const,
+    };
+
+    const formattedDate = dateObject.toLocaleDateString("en-US", options);
+
+    const dateComponents = formattedDate.split(" ");
+
+    const monthDay = dateComponents.slice(0, 2).join(" ").replace(",", "");
+
+    const year = dateComponents[2];
+
+    return { monthDay, year };
+};
+
+export const rgbDataURL = (r: number, g: number, b: number) => {
+    const keyStr =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+    const triplet = (e1: number, e2: number, e3: number) =>
+        keyStr.charAt(e1 >> 2) +
+        keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+        keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+        keyStr.charAt(e3 & 63);
+
+    return `data:image/gif;base64,R0lGODlhAQABAPAA${
+        triplet(0, r, g) + triplet(b, 255, 255)
+    }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+};
