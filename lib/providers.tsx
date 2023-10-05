a7c1d173c66b4357b0435cb961fcff4af4ceebc3
Adam Chong <adamkwchong@gmail.com>
2023-10-05 14:44:03 +0800

    add provider

---
+++ lib/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { FC, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
+import { NextIntlClientProvider } from "next-intl";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Providers: FC<LayoutProps> = ({ children }) => {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SessionProvider>{children}</SessionProvider>
+        </QueryClientProvider>
+    );
+};
+
+export default Providers;
