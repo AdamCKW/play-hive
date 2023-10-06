c3a8874b5aa702b7bd4a5b2a2569173e3493064e
Adam Chong <adamkwchong@gmail.com>
2023-10-06 11:15:05 +0800

    add image domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,41 @@ const withNextIntl = require("next-intl/plugin")(
     "./i18n.ts",
 );
 
-const nextConfig = withNextIntl({});
+const nextConfig = withNextIntl({
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "*.googleusercontent.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "*.gamespot.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "uploadthing.com",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+                port: "",
+            },
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+            },
+        ],
+    },
+    reactStrictMode: true,
+});
 
 module.exports = nextConfig;
