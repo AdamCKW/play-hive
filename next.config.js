e7e875527d7e6504f04d662f750315b12a479d72
Adam Chong <adamkwchong@gmail.com>
2023-10-06 15:39:12 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,53 @@
+// /** @type {import('next').NextConfig} */
+
+// const withNextIntl = require("next-intl/plugin")("./i18n.ts");
+
+// const nextConfig = withNextIntl({
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: "https",
+//                 hostname: "images.unsplash.com",
+//                 port: "",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "*.googleusercontent.com",
+//                 port: "",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "*.gamespot.com",
+//                 port: "",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "uploadthing.com",
+//                 port: "",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "picsum.photos",
+//                 port: "",
+//             },
+//             {
+//                 protocol: "https",
+//                 hostname: "utfs.io",
+//             },
+//         ],
+//     },
+//     reactStrictMode: true,
+// });
+
+module.exports = nextConfig;
+
+const withNextIntl = require("next-intl/plugin")("./i18n.ts");
+
 /** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const withNextIntl = require("next-intl/plugin")(
-    // This is the default (also the `src` folder is supported out of the box)
-    "./i18n.ts",
-);
-
-const nextConfig = withNextIntl({
+module.exports = withNextIntl({
+    nextConfig,
     images: {
         remotePatterns: [
             {
@@ -39,7 +81,4 @@ const nextConfig = withNextIntl({
             },
         ],
     },
-    reactStrictMode: true,
 });
-
-module.exports = nextConfig;
