7aab2667ac1158d1b2cc0a8e4b695a9a393a51d6
Adam Chong <adamkwchong@gmail.com>
2023-10-06 16:07:52 +0800

    add forget password page

--- app/[locale]/(auth)/reset-password/page.tsx
+++ app/[locale]/(auth)/reset-password/page.tsx
@@ -9,11 +9,24 @@ import {
 } from "@/components/ui/card";
 import { ResetPasswordForm } from "@/components/auth/reset-password-form";
 import { useTranslations } from "next-intl";
+import { getTranslator } from "next-intl/server";
 
-export const metadata: Metadata = {
-    title: "Reset Password",
-    description: "Enter your email to reset your password",
-};
+// export const metadata: Metadata = {
+//     title: "Forgot Password",
+//     description: "Enter your email to reset your password",
+// };
+
+type ExtendMetadata = Metadata & { params: { locale: string } };
+
+export async function generateMetadata({ params: { locale } }: ExtendMetadata) {
+    const t = await getTranslator(locale, "metadata.reset-password");
+
+    return {
+        title: t("title"),
+        description: t("description"),
+        icons: [{ rel: "icon", url: "images/logo.png" }],
+    };
+}
 
 export default function ResetPasswordPage() {
     const tPage = useTranslations("auth.reset-password.page");
