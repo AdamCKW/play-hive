43bcd3bef97049954c44c977670112f643a11f65
Adam Chong <adamkwchong@gmail.com>
2023-10-06 11:18:43 +0800

    add login page

---
+++ app/[locale]/(auth)/sign-in/page.tsx
@@ -0,0 +1,93 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
+import { linksConfig, siteConfig } from "@/config/site";
+import { getAuthSession } from "@/lib/auth";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { OAuthSignIn } from "@/components/auth/oauth-signin";
+import { UserLoginForm } from "@/components/auth/user-login-form";
+import { Icons } from "@/components/icons";
+import { getTranslator } from "next-intl/server";
+import { cn } from "@/lib/utils";
+import { Shell } from "@/components/shell";
+
+export const metadata: Metadata = {
+    title: "Login",
+    description: "Login Page",
+};
+
+interface LoginPageProps {
+    params: {
+        locale: string;
+    };
+}
+
+export default async function LoginPage({
+    params: { locale },
+}: LoginPageProps) {
+    const session = await getAuthSession();
+    const translate = await getTranslator(locale, "auth.login.page");
+
+    if (session?.user) redirect("/");
+
+    return (
+        <section className="container grid items-center gap-8 pb-8 pt-6 md:py-8 max-w-lg">
+            <Card className="md:border-none md:shadow-none">
+                <CardHeader className="space-y-1 text-center">
+                    <Icons.command className="mx-auto h-6 w-6" />
+                    <h1 className="text-2xl font-semibold tracking-tight">
+                        {translate("heading")}
+                    </h1>
+                    <p className="text-muted-foreground text-sm">
+                        {translate("subheading")}
+                    </p>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <UserLoginForm />
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <span className="w-full border-t" />
+                        </div>
+                        <div className="relative flex justify-center text-xs uppercase">
+                            <span className="bg-background text-muted-foreground px-2">
+                                {translate("continueWith")}
+                            </span>
+                        </div>
+                    </div>
+                    <OAuthSignIn />
+                </CardContent>
+                <CardFooter className="flex flex-wrap items-center justify-between gap-2">
+                    <Link
+                        href={
+                            linksConfig.signUp.disabled
+                                ? "#"
+                                : linksConfig.signUp.href
+                        }
+                        className="text-primary text-sm underline-offset-4 transition-colors hover:underline"
+                    >
+                        {translate("newTo", { siteName: siteConfig.name })}
+                    </Link>
+
+                    <Link
+                        href={
+                            linksConfig.forgotPassword.disabled
+                                ? "#"
+                                : linksConfig.forgotPassword.href
+                        }
+                        className="text-primary text-sm underline-offset-4 transition-colors hover:underline"
+                    >
+                        {translate("forgotPassword")}
+                    </Link>
+                </CardFooter>
+            </Card>
+        </section>
+    );
+}
---
+++ components/auth/user-login-form.tsx
@@ -0,0 +1,195 @@
+"use client";
+
+import * as React from "react";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { cn } from "@/lib/utils";
+import { LoginRequest, LoginValidation } from "@/lib/validators/sign-in";
+import { Button, buttonVariants } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "@/hooks/use-toast";
+import { Icons } from "@/components/icons";
+import { useTranslations } from "next-intl";
+import { linksConfig } from "@/config/site";
+
+interface UserLoginFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserLoginForm({ className, ...props }: UserLoginFormProps) {
+    const router = useRouter();
+
+    const [isLoading, setIsLoading] = React.useState<boolean>(false);
+    const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
+    const tValidation = useTranslations("auth.login.validation");
+    const tForm = useTranslations("auth.login.form");
+    const tError = useTranslations("error");
+
+    const validationMessages: Parameters<typeof LoginValidation> = [
+        tValidation("username_required"),
+        tValidation("username_refine"),
+        tValidation("password_required"),
+    ];
+
+    const form = useForm<LoginRequest>({
+        resolver: zodResolver(LoginValidation(...validationMessages)),
+        defaultValues: {
+            username: "",
+            password: "",
+        },
+    });
+
+    async function onSubmit(values: LoginRequest) {
+        setIsLoading(true);
+
+        const signInResponse = await signIn("credentials", {
+            username: values.username,
+            password: values.password,
+            redirect: false,
+            callbackUrl: linksConfig.home.href,
+        });
+
+        setIsLoading(false);
+
+        if (signInResponse?.error) {
+            return toast({
+                title: tError("heading.500"),
+                description: tError(signInResponse.error),
+            });
+        }
+
+        //TODO: temporary fix to force hard navigation
+        router.refresh();
+        return router.push(linksConfig.home.href);
+    }
+
+    return (
+        <div className={cn("grid gap-6", className)} {...props}>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className="grid gap-2">
+                        <div className="grid gap-1">
+                            <FormField
+                                control={form.control}
+                                name="username"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className="sr-only"
+                                            htmlFor="username"
+                                        >
+                                            {tForm("username.label")}
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                id="username"
+                                                placeholder={tForm(
+                                                    "username.placeholder",
+                                                )}
+                                                autoCapitalize="none"
+                                                autoComplete="username"
+                                                autoCorrect="off"
+                                                disabled={isLoading}
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="grid gap-1">
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel
+                                            className="sr-only"
+                                            htmlFor="password"
+                                        >
+                                            {tForm("password.label")}
+                                        </FormLabel>
+                                        <FormControl>
+                                            <div className="relative">
+                                                <Input
+                                                    id="password"
+                                                    type={
+                                                        showPassword
+                                                            ? "text"
+                                                            : "password"
+                                                    }
+                                                    placeholder={tForm(
+                                                        "password.placeholder",
+                                                    )}
+                                                    autoComplete="current-password"
+                                                    autoCorrect="off"
+                                                    className="pr-10"
+                                                    disabled={isLoading}
+                                                    {...field}
+                                                />
+                                                <Button
+                                                    type="button"
+                                                    variant="link"
+                                                    className="text-muted-foreground absolute right-0 top-0 h-full hover:text-current"
+                                                    onClick={() =>
+                                                        setShowPassword(
+                                                            (prev) => !prev,
+                                                        )
+                                                    }
+                                                >
+                                                    {showPassword ? (
+                                                        <Icons.eyeOff
+                                                            className="h-4 w-4"
+                                                            aria-hidden="true"
+                                                        />
+                                                    ) : (
+                                                        <Icons.eye
+                                                            className="h-4 w-4"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )}
+
+                                                    <span className="sr-only">
+                                                        {showPassword
+                                                            ? tForm(
+                                                                  "srPassword.hide",
+                                                              )
+                                                            : tForm(
+                                                                  "srPassword.show",
+                                                              )}
+                                                    </span>
+                                                </Button>
+                                            </div>
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <Button disabled={isLoading} className="capitalize">
+                            {isLoading && (
+                                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                            )}
+                            {tForm("signIn")}
+                        </Button>
+                    </div>
+                </form>
+            </Form>
+        </div>
+    );
+}
