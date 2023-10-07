1e407bea5c3ae7e025647ca2efde5abb3cd85317
Adam Chong <adamkwchong@gmail.com>
2023-10-07 16:34:50 +0800

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,6 +50,7 @@ module.exports = {
                     DEFAULT: "hsl(var(--card))",
                     foreground: "hsl(var(--card-foreground))",
                 },
+                gradient: "var(--gradient)",
             },
             borderRadius: {
                 lg: "var(--radius)",
