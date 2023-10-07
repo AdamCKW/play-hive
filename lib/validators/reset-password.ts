568d615f34044a60e720eb280474293325a0f922
Adam Chong <adamkwchong@gmail.com>
2023-10-07 13:32:52 +0800

    update reset password schema

--- lib/validators/reset-password.ts
+++ lib/validators/reset-password.ts
@@ -19,4 +19,35 @@ export const EmailValidation = (
             }),
     });
 
+export const NewPasswordValidation = (
+    password_min?: string,
+    password_max?: string,
+    password_refine?: string,
+) =>
+    z.object({
+        password: z
+            .string()
+            .min(8, {
+                message:
+                    password_min ||
+                    "Password must be at least 8 characters long",
+            })
+            .max(20, {
+                message:
+                    password_max ||
+                    "Password must be less than 20 characters long",
+            })
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*()]+$/,
+                {
+                    message:
+                        password_refine ||
+                        "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character (no space)",
+                },
+            ),
+    });
+
 export type ResetRequest = z.infer<ReturnType<typeof EmailValidation>>;
+export type NewPasswordRequest = z.infer<
+    ReturnType<typeof NewPasswordValidation>
+>;
