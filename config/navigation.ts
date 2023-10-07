2c8e99dfaa2ddaf81d60ea6158243fb43c6a1881
Adam Chong <adamkwchong@gmail.com>
2023-10-07 18:09:44 +0800

    add navigation links

---
+++ config/navigation.ts
@@ -0,0 +1,45 @@
+import { NavItem } from "@/types";
+import { linksConfig } from "./site";
+
+const {
+    home,
+    signIn,
+    signUp,
+    forgotPassword,
+    terms,
+    privacy,
+    cookie,
+    ads,
+    accessibility,
+    about,
+    discover,
+    news,
+    communities,
+    messages,
+    profile,
+} = linksConfig;
+
+export const NavBarItems: NavItem[] = [
+    {
+        ...profile,
+    },
+];
+
+export const LeftBarItems: NavItem[] = [
+    { ...home },
+    { ...discover },
+    { ...news },
+    { ...communities },
+    { ...messages },
+];
+
+export const RightBarItems: NavItem[] = [
+    { ...terms },
+    { ...privacy },
+    { ...cookie },
+    { ...ads },
+    { ...accessibility },
+    { ...about },
+];
+
+export const UserNavItems: NavItem[] = [];
