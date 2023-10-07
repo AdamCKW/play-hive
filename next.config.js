709da14f6a4e1647d6e3db3691fe0516e996c0bb
Adam Chong <adamkwchong@gmail.com>
2023-10-07 13:29:37 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,53 +1,8 @@
-// /** @type {import('next').NextConfig} */
-
-// const withNextIntl = require("next-intl/plugin")("./i18n.ts");
-
-// const nextConfig = withNextIntl({
-//     images: {
-//         remotePatterns: [
-//             {
-//                 protocol: "https",
-//                 hostname: "images.unsplash.com",
-//                 port: "",
-//             },
-//             {
-//                 protocol: "https",
-//                 hostname: "*.googleusercontent.com",
-//                 port: "",
-//             },
-//             {
-//                 protocol: "https",
-//                 hostname: "*.gamespot.com",
-//                 port: "",
-//             },
-//             {
-//                 protocol: "https",
-//                 hostname: "uploadthing.com",
-//                 port: "",
-//             },
-//             {
-//                 protocol: "https",
-//                 hostname: "picsum.photos",
-//                 port: "",
-//             },
-//             {
-//                 protocol: "https",
-//                 hostname: "utfs.io",
-//             },
-//         ],
-//     },
-//     reactStrictMode: true,
-// });
-
-// module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
 
 const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = withNextIntl({
-    nextConfig,
+const nextConfig = withNextIntl({
     images: {
         remotePatterns: [
             {
@@ -81,4 +36,7 @@ module.exports = withNextIntl({
             },
         ],
     },
+    reactStrictMode: true,
 });
+
+module.exports = nextConfig;
