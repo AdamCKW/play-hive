18c0312952e4ba4a8b17d963af266b6b0a0a1e2d
Adam Chong <adamkwchong@gmail.com>
2023-10-07 16:55:40 +0800

    add intl hook

---
+++ hooks/use-intl.ts
@@ -0,0 +1,10 @@
+import { useLocale } from "next-intl";
+import { useRouter, usePathname } from "next-intl/client";
+
+export const useIntl = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const locale = useLocale();
+
+    return { router, pathname, locale };
+};
