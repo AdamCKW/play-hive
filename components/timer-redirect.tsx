70b185772edc3fc448bc818ceff6ffa1fae09943
Adam Chong <adamkwchong@gmail.com>
2023-10-07 13:50:53 +0800

    add timer

---
+++ components/timer-redirect.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useInterval } from "@mantine/hooks";
+import { linksConfig } from "@/config/site";
+import { Loader2 } from "lucide-react";
+import { startTransition } from "react";
+import { toast } from "@/hooks/use-toast";
+import { useTranslations } from "next-intl";
+
+const TimerRedirect = () => {
+    const router = useRouter();
+    const tVerification = useTranslations("verification");
+    const [seconds, setSeconds] = useState(3);
+    const interval = useInterval(() => setSeconds((s) => s - 1), 1000);
+
+    // useEffect(() => {
+    //     interval.start();
+
+    //     const timer = setTimeout(() => {
+    //         router.push("/");
+    //     }, 3000);
+
+    //     return () => clearTimeout(timer);
+    // }, []);
+    useEffect(() => {
+        interval.start();
+
+        // Set a timeout to redirect after 3 seconds
+        const timer = setTimeout(() => {
+            router.replace(linksConfig.signIn.href);
+            interval.stop();
+        }, 3000); // 3000 milliseconds = 3 seconds
+
+        // Clear the timeout when the component unmounts
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center">
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            {tVerification("success.title", { count: seconds })}
+        </div>
+    );
+};
+
+export default TimerRedirect;
