2a576436f3918b1cb1ba4af1a81735d52b17f4e1
Adam Chong <adamkwchong@gmail.com>
2023-10-11 10:16:14 +0800

    uncomment code

--- app/[locale]/(root)/page.tsx
+++ app/[locale]/(root)/page.tsx
@@ -7,87 +7,104 @@ import Image from "next/image";
 import { notFound, redirect } from "next/navigation";
 import { Fragment } from "react";
 import MainFeed from "@/components/posts/feeds/main-feed";
+import { linksConfig } from "@/config/site";
+import { getTranslator } from "next-intl/server";
 
-export default async function Home() {
+interface HomePageProps {
+    params: {
+        locale: string;
+    };
+}
+
+export default async function Home({ params }: HomePageProps) {
     const session = await getAuthSession();
+    const t = await getTranslator(params.locale, "root.posts");
 
-    if (!session) redirect("/sign-up");
+    if (!session) redirect(linksConfig.signIn.href);
+    const getUser = await db.user.findUnique({
+        where: { id: session.user.id },
+        include: {
+            following: true,
+        },
+    });
 
-    // console.log(session);
-    // const getUser = await db.user.findUnique({
-    //     where: { id: session.user.id },
-    //     include: {
-    //         following: true,
-    //     },
-    // });
+    const followedUserIds = getUser?.following.map((follow) => follow.id);
 
-    // const followedUserIds = getUser?.following.map((follow) => follow.id);
+    const whereClause = {
+        parent: null,
+        AND: [
+            {
+                OR: [
+                    {
+                        author: {
+                            id: session.user.id,
+                        },
+                    },
+                    {
+                        author: {
+                            id: {
+                                in: followedUserIds,
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    };
 
-    // const whereClause = {
-    //     parent: null,
-    //     AND: [
-    //         {
-    //             OR: [
-    //                 {
-    //                     author: {
-    //                         id: session.user.id,
-    //                     },
-    //                 },
-    //                 {
-    //                     author: {
-    //                         id: {
-    //                             in: followedUserIds,
-    //                         },
-    //                     },
-    //                 },
-    //             ],
-    //         },
-    //     ],
-    // };
+    const data = await db.post.findMany({
+        where: whereClause,
+        include: {
+            community: true,
+            author: {
+                select: {
+                    id: true,
+                    name: true,
+                    username: true,
+                    image: true,
+                    bio: true,
+                },
+            },
+            children: {
+                include: {
+                    author: {
+                        select: {
+                            id: true,
+                            name: true,
+                            username: true,
+                            image: true,
+                            bio: true,
+                        },
+                    },
+                },
+            },
+            parent: true,
+            likes:
+                session.user.id == null
+                    ? false
+                    : { where: { userId: session.user.id } },
+            images: true,
+            _count: { select: { likes: true, children: true } },
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+        take: INFINITE_SCROLL_PAGINATION_RESULTS,
+    });
 
-    // const data = await db.post.findMany({
-    //     where: whereClause,
-    //     include: {
-    //         community: true,
-    //         author: {
-    //             select: {
-    //                 id: true,
-    //                 name: true,
-    //                 username: true,
-    //                 image: true,
-    //             },
-    //         },
-    //         children: {
-    //             include: {
-    //                 author: {
-    //                     select: {
-    //                         id: true,
-    //                         name: true,
-    //                         username: true,
-    //                         image: true,
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //         parent: true,
-    //         likes:
-    //             session.user.id == null
-    //                 ? false
-    //                 : { where: { userId: session.user.id } },
-    //         images: true,
-    //         _count: { select: { likes: true, children: true } },
-    //     },
-    //     orderBy: {
-    //         createdAt: "desc",
-    //     },
-    //     take: INFINITE_SCROLL_PAGINATION_RESULTS,
-    // });
-
-    // const posts: IPost[] = data.map(transformObject);
+    const posts: IPost[] = data.map(transformObject);
 
     return (
         <>
-            <h1 className="text-9xl">Main Page</h1>
+            {posts.length === 0 ? (
+                <div className="mt-4 text-center leading-loose text-neutral-600">
+                    {t("empty")}
+                </div>
+            ) : (
+                <div className="2xl:mx-4">
+                    <MainFeed initialPosts={posts} />
+                </div>
+            )}
         </>
     );
 }
