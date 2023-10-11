28cce92fb374b35be102e4678e00a03d7f68c776
Adam Chong <adamkwchong@gmail.com>
2023-10-11 12:43:51 +0800

    update translation

---
+++ app/api/auth/forgot-password/[id]/route.ts
@@ -0,0 +1,63 @@
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { NewPasswordValidation } from "@/lib/validators/reset-password";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+import { decryptId } from "@/lib/utils";
+
+export async function PATCH(
+    req: NextRequest,
+    { params }: { params: { id: string } },
+) {
+    try {
+        const { id } = params;
+
+        const body = await req.json();
+
+        if (!id) {
+            return new NextResponse("reset.failed.params_empty", {
+                status: 404,
+            });
+        }
+
+        const userId = decryptId(id);
+
+        const user = await db.user.findUnique({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            return new NextResponse("reset.failed.user_not_found", {
+                status: 404,
+            });
+        }
+
+        const validation = NewPasswordValidation();
+        const { password } = validation.parse(body);
+
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const updatedUser = await db.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                password: hashedPassword,
+                forgotPasswordToken: null,
+                forgotPasswordExpiry: null,
+            },
+        });
+
+        return new NextResponse("Success", { status: 200 });
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new NextResponse(error.message, { status: 400 });
+        }
+
+        console.log("PATCH /api/auth/forgot-password/[id]", error);
+
+        return new NextResponse("500.internal_error", { status: 500 });
+    }
+}
