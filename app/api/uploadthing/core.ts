19dcc9a7fd1f324254922e0be283cd089efc8b42
Adam Chong <adamkwchong@gmail.com>
2023-10-11 13:50:18 +0800

    add uploadthing api

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,38 @@
+import { getAuthSession } from "@/lib/auth";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+const handleAuth = async () => {
+    const session = await getAuthSession();
+
+    if (!session) throw new Error("401.unauthorized");
+
+    return { userId: session.user.id };
+};
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+    imageUploader: f({
+        image: { maxFileSize: "8MB", maxFileCount: 4 },
+    })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+
+    userAvatar: f({
+        image: { maxFileSize: "8MB", maxFileCount: 1 },
+    })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+    userBanner: f({
+        image: { maxFileSize: "8MB", maxFileCount: 1 },
+    })
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+
+    messageFile: f(["image"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
