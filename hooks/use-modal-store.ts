8f9a494acefd201e3908891c3c97f169b61e0f83
Adam Chong <adamkwchong@gmail.com>
2023-10-11 13:19:34 +0800

    add modal store

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,30 @@
+import { create } from "zustand";
+
+export type ModalType =
+    | "users"
+    | "editProfile"
+    | "messageFile"
+    | "deleteMessage"
+    | "imageModal";
+
+interface ModalData {
+    apiUrl?: string;
+    query?: Record<string, any>;
+    imageUrl?: string;
+}
+
+interface ModalStore {
+    type: ModalType | null;
+    data: ModalData;
+    isOpen: boolean;
+    onOpen: (type: ModalType, data?: ModalData) => void;
+    onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+    type: null,
+    data: {},
+    isOpen: false,
+    onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+    onClose: () => set({ type: null, isOpen: false }),
+}));
