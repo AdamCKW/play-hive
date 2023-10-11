e346951eb86fa487d9feec2083088b592e08452f
Adam Chong <adamkwchong@gmail.com>
2023-10-11 15:40:36 +0800

    add controls

---
+++ components/posts/controls/index.tsx
@@ -0,0 +1,35 @@
+import { Prisma } from '@prisma/client'
+
+import { IPost } from '@/types/db'
+
+import CommentModal from '../comment/comment-modal'
+import Like from './like'
+import ShareButton from './share'
+
+interface ControlsProps {
+    data: IPost
+    single?: boolean
+    queryKey?: string[]
+}
+
+export default function Controls({
+    data,
+    single = false,
+    queryKey,
+}: ControlsProps) {
+    return (
+        <div className="relative h-9">
+            <div className="absolute left-0 top-0 z-10 flex items-center space-x-3.5 py-2">
+                <Like
+                    postId={data.id}
+                    likedByUser={data.likedByUser}
+                    single={single}
+                    queryKey={queryKey}
+                />
+
+                <CommentModal data={data} single={single} queryKey={queryKey} />
+                <ShareButton name={data.author.name || ''} post={data.id} />
+            </div>
+        </div>
+    )
+}
