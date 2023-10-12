cfeb73bd762d8d041fabc91375c55f5b2abdf179
Adam Chong <adamkwchong@gmail.com>
2023-10-12 16:15:19 +0800

    add profile api

---
+++ app/api/users/[id]/posts/route.ts
@@ -0,0 +1,83 @@
+import { revalidatePath } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { transformObject } from "@/lib/utils";
+
+export async function GET(
+    req: NextRequest,
+    { params }: { params: { id: string } },
+) {
+    try {
+        const { id } = params;
+        const url = new URL(req.url);
+        const session = await getAuthSession();
+        if (!session?.user) {
+            return new NextResponse("401.unauthorized", { status: 401 });
+        }
+
+        const { limit, page } = z
+            .object({
+                limit: z.string(),
+                page: z.string(),
+            })
+            .parse({
+                limit: url.searchParams.get("limit"),
+                page: url.searchParams.get("page"),
+            });
+
+        const response = await db.post.findMany({
+            take: parseInt(limit),
+            skip: (parseInt(page) - 1) * parseInt(limit),
+            where: {
+                authorId: id,
+                parent: null,
+                deleted: false,
+            },
+            include: {
+                author: {
+                    select: {
+                        id: true,
+                        name: true,
+                        username: true,
+                        image: true,
+                    },
+                },
+                children: {
+                    include: {
+                        author: {
+                            select: {
+                                id: true,
+                                name: true,
+                                username: true,
+                                image: true,
+                            },
+                        },
+                    },
+                },
+                parent: true,
+                images: true,
+                likes:
+                    session.user.id == null
+                        ? false
+                        : { where: { userId: session.user.id } },
+                _count: { select: { likes: true, children: true } },
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+        });
+
+        const posts = response.map(transformObject);
+
+        return NextResponse.json(posts);
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return new NextResponse(error.message, { status: 400 });
+        }
+        console.log("ERROR in GET /api/users/[id]/posts: ", error);
+        return new NextResponse("500.internal_error", { status: 500 });
+    }
+}
