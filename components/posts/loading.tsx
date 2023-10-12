e60dcd98af0989923b9860f5fc2f8d51cc74e180
Adam Chong <adamkwchong@gmail.com>
2023-10-12 18:21:25 +0800

    update loading

--- components/posts/loading.tsx
+++ components/posts/loading.tsx
@@ -2,8 +2,10 @@ import { Loader2 } from "lucide-react";
 
 export function PostLoading() {
     return (
-        <div className="flex w-full justify-center py-4">
-            <Loader2 className="text-muted-foreground h-4 w-4 animate-spin" />
+        <div className="2xl:mx-4">
+            <div className="flex w-full justify-center py-4">
+                <Loader2 className="text-muted-foreground h-4 w-4 animate-spin" />
+            </div>
         </div>
     );
 }
