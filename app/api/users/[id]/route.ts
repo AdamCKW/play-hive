2e1e4c3322b74f8a147cfce2c05a0fb113990c12
Adam Chong <adamkwchong@gmail.com>
2023-10-12 15:12:33 +0800

    fix edit profile route

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 import { FullEditValidation } from "@/lib/validators/edit-profile";
 import z from "zod";
 import { decryptId } from "@/lib/utils";
+import { User as UserData } from "@prisma/client";
 
 export async function PATCH(
     req: NextRequest,
@@ -18,79 +19,73 @@ export async function PATCH(
             return new NextResponse("401.unauthorized", { status: 401 });
 
         const { id } = params;
-
-        const userId = decryptId(id);
         const body = await req.json();
+        const userId = decryptId(id);
 
-        if (session.user.id !== id)
+        if (session.user.id !== userId)
             return new NextResponse("401.unauthorized", { status: 401 });
 
         const { username, name, email, bio, imageUrl, imageBanner, password } =
             FullEditValidation().parse(body);
 
-        const getUser = await db.user.findUnique({
+        const user = await db.user.findUnique({
             where: { id: userId },
         });
-
-        if (!getUser)
-            return new NextResponse("404.user_not_found", { status: 404 });
 
         const getAccount = await db.account.findFirst({
             where: { userId },
         });
 
-        const findExistingUsername = await db.user.findFirst({
-            where: { username },
-        });
+        if (!user) {
+            return new NextResponse("404.user_not_found", { status: 404 });
+        }
 
-        if (findExistingUsername)
-            return new NextResponse("settings.failed.username_exist", {
-                status: 400,
+        const userData: {
+            username?: string;
+            name?: string;
+            email?: string;
+            bio?: string;
+            image?: string;
+            cover?: string;
+            password?: string;
+        } = {};
+
+        if (username !== user.username) {
+            const findExistingUsername = await db.user.findFirst({
+                where: { username },
             });
 
-        if (getAccount) {
-            await db.user.update({
-                where: { id: userId },
-                data: {
-                    username,
-                    name,
-                    bio,
-                    image: imageUrl,
-                    cover: imageBanner,
-                },
-            });
-        } else {
-            if (!password && password !== undefined) {
-                console.log(password);
-                const salt = await bcrypt.genSalt(11);
-                const hashedPassword = await bcrypt.hash(password, salt);
-
-                await db.user.update({
-                    where: { id: userId },
-                    data: {
-                        username,
-                        name,
-                        bio,
-                        email,
-                        image: imageUrl,
-                        cover: imageBanner,
-                        password: hashedPassword,
-                    },
-                });
-            } else {
-                await db.user.update({
-                    where: { id: userId },
-                    data: {
-                        username,
-                        name,
-                        bio,
-                        email,
-                        image: imageUrl,
-                        cover: imageBanner,
-                    },
+            if (findExistingUsername) {
+                return new NextResponse("settings.failed.username_exist", {
+                    status: 400,
                 });
             }
+
+            userData.username = username;
         }
+        if (name !== user.name) {
+            userData.name = name;
+        }
+        if (email !== user.email) {
+            userData.email = email;
+        }
+        if (bio !== user.bio) {
+            userData.bio = bio;
+        }
+        if (imageUrl !== user.image) {
+            userData.image = imageUrl;
+        }
+        if (imageBanner !== user.cover) {
+            userData.cover = imageBanner;
+        }
+        if (!getAccount && password && password !== undefined) {
+            const salt = await bcrypt.genSalt(11);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            userData.password = hashedPassword;
+        }
+
+        await db.user.update({ where: { id: userId }, data: userData });
 
         return new NextResponse("Success", { status: 200 });
     } catch (error) {
@@ -99,6 +94,8 @@ export async function PATCH(
                 status: 422,
             });
         }
+
+        console.log("ERROR in PATCH /api/users/[id]/:", error);
 
         return new NextResponse("500.internal_error", { status: 500 });
     }
