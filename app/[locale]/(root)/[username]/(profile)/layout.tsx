4f3db64ff8e5722090e65dfaa7b4ba1a609275b6
Adam Chong <adamkwchong@gmail.com>
2023-10-12 22:30:22 +0800

    update class

--- app/[locale]/(root)/[username]/(profile)/layout.tsx
+++ app/[locale]/(root)/[username]/(profile)/layout.tsx
@@ -104,12 +104,12 @@ export default async function ProfilePageLayout({
                         ) : null}
 
                         <div className="flex gap-2 py-4">
-                            <div className="text-neutral-600">
+                            <div className="text-muted-foreground">
                                 {nFormatter(getUser.following.length, 1)}{" "}
                                 {t("following")}
                             </div>
 
-                            <div className="text-neutral-600">
+                            <div className="text-muted-foreground">
                                 {nFormatter(getUser.followedBy.length, 1)}{" "}
                                 {getUser.followedBy.length === 1
                                     ? t("follower")
